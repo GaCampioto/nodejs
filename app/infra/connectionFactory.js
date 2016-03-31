5a3f24435d4edf5e7d12ac8dfecf9007da0ce8ac
Gabriel Campioto Pires <gcampioto@gmail.com>
2016-03-30 23:33:02 -0300

    Adding regex to hide the database information

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -2,11 +2,14 @@ var mysql = require('mysql');
 
 var createConnection = function(){
 	if (process.env.NODE_ENV == 'production'){
+		var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
+		var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
+		//POSICAO 0 SEMPRE É A EXPRESSÃO COMPLETA
 		return mysql.createConnection({
-			host:'localhost',
-			user:'root',
-			password:'root',
-			database:'casadocodigo_nodejs'
+			host:grupos[3],
+			user:grupos[1],
+			password:grupos[2],
+			database:grupos[4]
 		});
 	}
 
